384e4655092179bf6988d0914e706c6ae69d0b6b
Lee Gurley <leeisme@gmail.com>
2017-03-20 20:17:57 -0400

    SimpleRCD: Incremental updates to WPF Client.

---
+++ RCD.WPFClient/ValidationRules/IntegerValidationRule.cs
@@ -0,0 +1,24 @@
+﻿namespace RCD.WPFClient.ValidationRules {
+	
+	#region Directives
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Controls;
+	#endregion
+
+	public class IntegerValidationRule
+		: ValidationRule {
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+			if (value is int) {
+				return new ValidationResult(true, null);
+			}
+			return new ValidationResult(false, @"Integers only!");
+		}
+
+	}
+
+}
